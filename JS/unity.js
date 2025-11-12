22c67fd058cc806b99ed32cf402e879c468c7ea1
L1ttlEJ <125958416+L1ttlEJ@users.noreply.github.com>
2025-11-12 13:35:05 +0100

    up

--- JS/unity.js
+++ JS/unity.js
@@ -156,3 +156,15 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 5000);
     }
 });
+
+
+//NAV///
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navLinks = document.getElementById("nav-links");
+
+    menuToggle.addEventListener("click", () => {
+        menuToggle.classList.toggle("open");
+        navLinks.classList.toggle("open");
+    });
+});
