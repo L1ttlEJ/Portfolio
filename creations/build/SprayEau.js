91228e7cabc649a11ecc5b7f16b51ce3d44fd750
L1ttlEJ <125958416+L1ttlEJ@users.noreply.github.com>
2025-12-04 11:08:45 +0100

    fdzs

--- creations/build/SprayEau.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-class SprayEau extends Spray {
-    constructor(element, coordX, coordY) {
-        super(element, coordX, coordY);
-        this.setImage("Spray.png", 100, 100);
-    }
-}
--- creations/build/SprayInsecte.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-class SprayInsecte extends Spray {
-    constructor(element, coordX, coordY) {
-        super(element, coordX, coordY);
-        this.setImage("Spray2.png", 100, 100);
-    }
-}
